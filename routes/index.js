ae3fb9f7b9f99dc4d5c5c3e6eeeeb4eac3976256
omisteck <talk2omis@gmail.com>
2021-11-03 23:43:00 +0100

    sequelize setup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const models = require('../models');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/users', function(req, res, next) {
+  models.User.findAll().then((allUsers)=>{
+    res.json(allUsers)
+  });
+});
+
+router.post('/users', function(req, res, next) {
+  models.User.findAll().then((allUsers)=>{
+    res.json(allUsers)
+  });
 });
 
 module.exports = router;
