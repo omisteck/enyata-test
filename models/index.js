1bf26710a95484b6b928aa9afc6a4252c5f76e96
omisteck <talk2omis@gmail.com>
2021-11-05 15:22:08 +0100

    con v5

--- models/index.js
+++ models/index.js
@@ -11,8 +11,10 @@ const db = {};
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  console.log('test');
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
+  console.log('tes2');
 }
 
 fs
